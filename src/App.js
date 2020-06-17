f973a767bcf30a0184072c875954399d6ab3e24d
Zac Smith <zrs3141592@gmail.com>
2020-06-17 14:42:40 -0600

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,15 @@
 import React from 'react'
 import './App.css'
 import Header from './components/Header'
-import DashBoard from './components/Dashboard'
 import Dashboard from './components/Dashboard'
 
 const App = () => {
   return (
-    <>
-      <div className="container">
-        <h3>Conway Game of Life</h3>
-        <Header />
-        <Dashboard />
-      </div>
-    </>
+    <div className="container">
+      <h3>Conway Game of Life</h3>
+      <Header />
+      <Dashboard />
+    </div>
   )
 }
 
