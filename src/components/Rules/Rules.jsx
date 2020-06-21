7384c108018ec638f06b16ae67ca679962f7e692
Zac Smith <zrs3141592@gmail.com>
2020-06-20 21:58:26 -0600

    style and add rules

--- src/components/Rules/Rules.jsx
+++ src/components/Rules/Rules.jsx
@@ -4,34 +4,48 @@ const Rules = () => {
   return (
     <div className={styles.rules_container}>
       <h2>Rules</h2>
-      <ul>
+      <p className={styles.p}>
+        The universe of the Game of Life is an infinite, two-dimensional
+        orthogonal grid of square cells, each of which is in one of two possible
+        states, live or dead, (or populated and unpopulated, respectively).
+        Every cell interacts with its eight neighbours, which are the cells that
+        are horizontally, vertically, or diagonally adjacent. At each step in
+        time, the following transitions occur:
+      </p>
+      <ol className={styles.ol}>
         <li className={styles.li}>
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eius officia
-          laudantium, perferendis dolor similique laborum modi architecto
-          blanditiis
+          Any live cell with fewer than two live neighbours dies, as if by
+          underpopulation.
         </li>
         <li className={styles.li}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugiat nemo
-          et natus in soluta modi expedita eius quisquam omnis accusantium rem
-          temporibus,
+          Any live cell with two or three live neighbours lives on to the next
+          generation.
         </li>
         <li className={styles.li}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rem ipsum et
-          inventore nam laborum.
+          Any live cell with more than three live neighbours dies, as if by
+          overpopulation.
         </li>
         <li className={styles.li}>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus
-          modi voluptatibus reprehenderit laboriosam velit dolor dignissimos
+          Any dead cell with exactly three live neighbours becomes a live cell,
+          as if by reproduction.
+        </li>
+      </ol>
+      <p className={styles.p}>
+        These rules, which compare the behavior of the automaton to real life,
+        can be condensed into the following:
+      </p>
+      <ol className={styles.ol}>
+        <li className={styles.li}>
+          Any live cell with two or three live neighbours survives.
         </li>
         <li className={styles.li}>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus,
-          magni sunt nam dolore dolor recusandae nisi non debitis iste enim in
-          praesentium adipisci dolorem ratione at quasi neque esse
-          molestias.Mollitia nam illum dicta corrupti odit, expedita provident,
-          harum quia sit molestias fugiat, illo laudantium libero et perferendis
-          optio quam iusto earum iure unde aut cum neque. Expedita, qui quos!
+          Any dead cell with three live neighbours becomes a live cell.
         </li>
-      </ul>
+        <li className={styles.li}>
+          All other live cells die in the next generation. Similarly, all other
+          dead cells stay dead.
+        </li>
+      </ol>
     </div>
   )
 }
