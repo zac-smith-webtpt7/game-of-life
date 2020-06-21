0e7a268a56659ad7173b5c42bcca95052979cb8f
Zac Smith <zrs3141592@gmail.com>
2020-06-20 22:47:45 -0600

    basic grid structure

--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,10 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './GameBoard.module.css'
-import Game from '../Game/Game'
+
+const nRows = 50
+const nCols = 50
+
 const GameBoard = () => {
+  const [gameGrid, setGameGrid] = useState(() => {
+    const rows = []
+    for (let i = 0; i < nRows; i++) {
+      rows.push(Array.from(Array(nCols), () => 0))
+    }
+
+    return rows
+  })
+  // console.log('rows', rows)
   return (
-    <div className={styles.game_board}>
-      <h2>GAME GOES HERE</h2>
+    <div
+      className={styles.game_board}
+      style={{
+        display: 'grid',
+        gridTemplateColumns: `repeat(${nCols}, 10px)`,
+      }}
+    >
+      {gameGrid.map((rows, i) =>
+        rows.map((col, k) => (
+          <div
+            key={`${i}+${k}`}
+            style={{
+              width: 10,
+              height: 10,
+              background: gameGrid[i][k] ? 'pink' : undefined,
+              border: '1px solid black',
+            }}
+          />
+        ))
+      )}
     </div>
   )
 }
