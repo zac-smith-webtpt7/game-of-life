7511988b60e3198301eb22c16b8942ce2943e852
Zac Smith <zrs3141592@gmail.com>
2020-06-28 17:13:09 -0600

    add basic description of Game of Life

---
+++ src/components/About/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import styles from './About.module.css'
+
+const About = () => {
+  return (
+    <section>
+      <h3>John Conway's Game of Life</h3>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum,
+        facere assumenda. Quis rerum, libero iure suscipit autem sequi eligendi
+        commodi. Architecto temporibus nam ullam sed, aliquam omnis non ut
+        illum. Voluptate, aspernatur? Corporis facere officiis at asperiores
+        autem fugit quae nesciunt, omnis cupiditate inventore atque hic odit
+        blanditiis eligendi, accusamus quaerat perspiciatis repellat quos ipsa,
+        id numquam dicta maiores tempora. Amet veniam ad repellat? Omnis
+        blanditiis velit sapiente quam libero cum quod, impedit nostrum adipisci
+        maiores aut, asperiores neque exercitationem aliquam ipsa. Voluptatum
+        beatae error ipsam amet odio esse sit. Perspiciatis alias omnis odio
+        doloremque, illum, molestias vero obcaecati id velit vitae autem
+        quisquam, a iure maiores optio asperiores in repellendus harum nemo
+        facere accusamus debitis est. Accusamus, aliquam quas! Aperiam,
+        excepturi! Reiciendis ad dolorum perspiciatis consectetur pariatur,
+        maxime modi asperiores incidunt illum labore id tempora quaerat iste
+        nostrum sed voluptate cumque ratione reprehenderit fuga sint itaque,
+        maiores eos repellendus.
+      </p>
+    </section>
+  )
+}
+
+export default About
