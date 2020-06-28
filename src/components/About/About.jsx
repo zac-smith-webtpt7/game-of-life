7249c82be7c45f9d5209e2b18e7a2089ec09ede9
Zac Smith <zrs3141592@gmail.com>
2020-06-28 17:32:43 -0600

    add description of the development of rules

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,24 +5,32 @@ const About = () => {
   return (
     <section>
       <h3>John Conway's Game of Life</h3>
-      <p>
-        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Laborum,
-        facere assumenda. Quis rerum, libero iure suscipit autem sequi eligendi
-        commodi. Architecto temporibus nam ullam sed, aliquam omnis non ut
-        illum. Voluptate, aspernatur? Corporis facere officiis at asperiores
-        autem fugit quae nesciunt, omnis cupiditate inventore atque hic odit
-        blanditiis eligendi, accusamus quaerat perspiciatis repellat quos ipsa,
-        id numquam dicta maiores tempora. Amet veniam ad repellat? Omnis
-        blanditiis velit sapiente quam libero cum quod, impedit nostrum adipisci
-        maiores aut, asperiores neque exercitationem aliquam ipsa. Voluptatum
-        beatae error ipsam amet odio esse sit. Perspiciatis alias omnis odio
-        doloremque, illum, molestias vero obcaecati id velit vitae autem
-        quisquam, a iure maiores optio asperiores in repellendus harum nemo
-        facere accusamus debitis est. Accusamus, aliquam quas! Aperiam,
-        excepturi! Reiciendis ad dolorum perspiciatis consectetur pariatur,
-        maxime modi asperiores incidunt illum labore id tempora quaerat iste
-        nostrum sed voluptate cumque ratione reprehenderit fuga sint itaque,
-        maiores eos repellendus.
+      <p className={styles.top}>
+        Motivated by the mathematical logic of creation, as purposed by John von
+        Neumann in the 1940s, John Conway began to experiment with
+        two-dimensional cellular automation.
+      </p>
+      <p className={styles.pTag}>
+        John von Neumann defined life as a creation that can reproduce itself
+        and simulate a Turing machine. John von Neumann's ideas on the matter
+        stated turned out to be unrealistic witht he technology of the time, and
+        this lead Stanislaw Ulam to invent cellular automata. Cellular automata
+        was meant to simulate the methods proposed by von Neumann, and was a
+        construct of a two-dimensional grid. Although Ulam discussed using
+        computers to simulate his cellular automata, it was following
+        generations that took and modified his thoeries. In 1968 John Conway
+        started his research.
+      </p>
+      <p className={styles.pTag}>
+        Originally John Conway's goal with cellular automata was to define an
+        'interesting' and 'unpredictable' cell automation. John Conway wanted
+        some intial states to last a long time before dying while preventing
+        overcrowding, and others that will dye nearly immediately or last
+        seemingly forever. The Game of Life is his definition that meets John
+        von Nuemann's two general requirements. Altough John Conway's definition
+        is proof-oriented, he aimed for it to be simple with having to valide or
+        prove the automation was alive. He rules or definition are provide
+        above, and is the basis for this game.
       </p>
     </section>
   )
